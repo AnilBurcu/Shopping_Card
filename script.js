53baa9ca2b0f91df7fb6c004feb1e332322fedd6
AnilBurcu <anlbrc95@gmail.com>
2024-02-12 12:15:06 +0300

    variables

---
+++ script.js
@@ -0,0 +1,10 @@
+/* Variables */
+const cartBtn = document.querySelector(".cart-btn");
+const clearCartBtn = document.querySelector(".btn-clear");
+const cartItems = document.querySelector(".cart-items");
+const cartTotal = document.querySelector(".total-value");
+const cartContent = document.querySelector(".cart-list");
+const productsDOM = document.querySelector("#products-dom");
+
+console.log(clearCartBtn);
+
